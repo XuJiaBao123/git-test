5d76c5f09d31d354a9e8c4ced46a8d83f33aa85a
xujiabao <1910865580@qq.com>
2021-06-01 00:28:53 +0800

    first idea git

---
+++ src/main/java/com/xujiabao/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xujiabao.git;
+
+/**
+ * @Description:
+ * @Author: XuJiaBao
+ * @Date: 2021/6/1 0:24
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
